01a8bc9c7c119b83379dd193bc7706b9a073ca5c
Punam Sambare <sambarepunam90@gmail.com>
2026-04-09 22:01:17 +0530

    Implement bit reversal function in reverse_bits.java

--- coding/reverse_bits.java
+++ coding/reverse_bits.java
@@ -1,1 +1,20 @@
+class Solution {
+    public static int reverseBits(int n) {
+       
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;          // get last bit
+            result = (result << 1) | bit; // add to result
+            n = n >> 1;              // shift n
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 43261596;
+        System.out.println(reverseBits(n));
+    }
+}
 
