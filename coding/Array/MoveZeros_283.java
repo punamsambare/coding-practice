e420a631fb23de8094f44983b791d10c06a64c65
Punam Sambare <sambarepunam90@gmail.com>
2026-04-25 20:05:50 +0530

    LeetCode problem 283 Move Zeroes in place

---
+++ coding/Array/MoveZeros_283.java
@@ -0,0 +1,31 @@
+public class MoveZeros_283 {
+     public static void main(String[]args)
+     {
+        int nums[]={0,1,0,3,12};
+        moveZeroes(nums);
+
+        for (int num : nums)
+        {
+            System.out.println(num+" ");
+        }
+
+     }
+ 
+    public static void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++)
+        {  
+            if(nums[i]!=0)
+            {
+             nums[j]=nums[i];
+             j++;
+            }
+        }
+        while(j<nums.length)
+        {
+            nums[j]=0;
+            j++;
+        }
+    }
+}
+
