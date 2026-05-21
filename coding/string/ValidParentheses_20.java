1ff80f83cfab26403dd831da0dd6ac7059af983c
Punam Sambare <sambarepunam90@gmail.com>
2026-05-21 22:06:16 +0530

    LeetCode problem 20 Valid Parentheses

---
+++ coding/string/ValidParentheses_20.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class ValidParentheses_20 {
+    public static void main(String[]args)
+    {
+        String s="({})[]";
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+
+        for(int i=0;i<s.length();i++)
+        {   char ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='[')
+            {
+                stack.push(ch);
+            }
+            else if(ch==')')
+            {
+                if(stack.isEmpty()||stack.pop()!='(')
+                {
+                    return false;
+                }
+            }
+            else if(ch=='}')
+            {
+                if(stack.isEmpty()||stack.pop()!='{')
+                {
+                    return false;
+                }
+            }
+            else if(ch==']')
+            {
+                if(stack.isEmpty()||stack.pop()!='[')
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
