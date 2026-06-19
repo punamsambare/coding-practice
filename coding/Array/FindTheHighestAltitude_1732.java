273e9197efeca62e46b6ba6b9bd8f3fbfbac5067
Punam Sambare <sambarepunam90@gmail.com>
2026-06-19 22:15:00 +0530

    LeetCode Problem 1732 Find The Highest Altitude

---
+++ coding/Array/FindTheHighestAltitude_1732.java
@@ -0,0 +1,18 @@
+public class FindTheHighestAltitude_1732 {
+    public static void main(String[]args)
+    {
+        int[] gain={-5,1,5,0,-7};
+        System.out.println(largestAltitude(gain));
+    }
+    public static int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            altitude += g;
+            maxAltitude = Math.max(maxAltitude, altitude);
+        }
+
+        return maxAltitude;
+    }
+}
