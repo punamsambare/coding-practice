d458a48f315a710a6b24f673f84fb786b24ea55b
Punam Sambare <sambarepunam90@gmail.com>
2026-06-21 22:35:34 +0530

    LeetCode Problem 605 Can Place Flowers

---
+++ coding/Array/CanPlaceFlowers_605.java
@@ -0,0 +1,28 @@
+public class CanPlaceFlowers_605 {
+    public static void main(String[] args) {
+         int[] flowerbed = {1, 0, 0, 0, 1};
+        int n = 1;
+
+        System.out.println(canPlaceFlowers(flowerbed, n));
+    }
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if (flowerbed[i] == 0) {
+                boolean leftEmpty = (i == 0 || flowerbed[i - 1] == 0);
+                boolean rightEmpty = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
+
+                if (leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1; 
+                    n--;
+
+                    if (n == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return n <= 0;
+    }
+}
