aa0f1be6b26b192ec28405a1a4c94936ee054201
Punam Sambare <sambarepunam90@gmail.com>
2026-06-22 20:43:25 +0530

    LeetCode problem 1189 Maximum number of balloons

---
+++ coding/string/MaximumNumberOfBalloon_1189.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class MaximumNumberOfBalloon_1189 {
+    public static void main(String[] args) {
+        String text="nlaebolko";
+        System.out.println(maxNumberOfBalloons(text));
+    }
+    public static int maxNumberOfBalloons(String text) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : text.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int b = map.getOrDefault('b', 0);
+        int a = map.getOrDefault('a', 0);
+        int l = map.getOrDefault('l', 0) / 2;
+        int o = map.getOrDefault('o', 0) / 2;
+        int n = map.getOrDefault('n', 0);
+
+        return Math.min(Math.min(b, a),
+                Math.min(Math.min(l, o), n));
+    }
+}
