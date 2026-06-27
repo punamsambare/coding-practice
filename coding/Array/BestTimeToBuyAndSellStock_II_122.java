cb2dafe08988b81fcea625ce480c33f153076183
Punam Sambare <sambarepunam90@gmail.com>
2026-06-27 21:44:24 +0530

    LeetCode problem 122 Best Time To Buy And Sell  Stock II

---
+++ coding/Array/BestTimeToBuyAndSellStock_II_122.java
@@ -0,0 +1,19 @@
+public class BestTimeToBuyAndSellStock_II_122 {
+   public static void main(String[] args)
+   {
+      int[] prices ={7,1,5,3,6,4};
+      System.out.println(maxProfit(prices));
+   }
+    public static int maxProfit(int[] prices)
+    {
+        int profit=0;
+        for(int i = 1; i < prices.length; i++)
+        {
+            if(prices[i] > prices[i-1])
+            {
+                profit = profit + prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
