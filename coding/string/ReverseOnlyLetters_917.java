27034856ce3bb174206716005df647d674cdca3d
Punam Sambare <sambarepunam90@gmail.com>
2026-07-13 22:12:46 +0530

    LeetCode problem 917 Reverse only letters

---
+++ coding/string/ReverseOnlyLetters_917.java
@@ -0,0 +1,31 @@
+public class ReverseOnlyLetters_917 {
+    public static void main(String[] args){
+        String s = "ab-Cd";
+        System.out.println(reverseOnlyLetters(s));
+    }
+    public static String reverseOnlyLetters(String s) {
+        char[] arr =s.toCharArray();
+
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left < right) {
+
+            if(!Character.isLetter(arr[left])){
+                left++;
+            }
+            else if(!Character.isLetter(arr[right])){
+                right--;
+            }
+            else{
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+}
