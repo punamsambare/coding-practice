f333743a166eb6de11b236373279a28711695736
Punam Sambare <sambarepunam90@gmail.com>
2026-07-21 18:37:18 +0530

    LeetCode problem 392 Is Subsequence

---
+++ coding/string/IsSubsequence_392.java
@@ -0,0 +1,27 @@
+public class IsSubsequence_392 {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbjdc";
+
+        System.out.println(isSubsequence(s,t));
+
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        return i == s.length();
+    }
+}
