354cd8e71c392f7ba4a7ed5a48e97e3323926d71
Punam Sambare <sambarepunam90@gmail.com>
2026-08-16 22:03:50 +0530

    LeetCode problem 674 Longest Continuous Increasing Subsequence

---
+++ coding/Array/LongestContinuousIncreasingSubsequence_674.java
@@ -0,0 +1,24 @@
+public class LongestContinuousIncreasingSubsequence_674 {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,4,7};
+
+        System.out.println(findLengthOfLCIS(nums));
+    }
+    public static int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        int maxLength = 1;
+        int currentLength = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                currentLength++;
+                maxLength = Math.max(maxLength, currentLength);
+            } else {
+                currentLength = 1;
+            }
+        }
+        
+        return maxLength;
+    }
+}
