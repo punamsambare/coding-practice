b866ed581eac021dc8f4a140cfc22aec6c79e88f
Punam Sambare <sambarepunam90@gmail.com>
2026-09-01 15:19:41 +0530

    LeetCode  problem 263 Ugly Number

---
+++ coding/Math/UglyNumber_263.java
@@ -0,0 +1,23 @@
+public class UglyNumber_263 {
+    public static void main(String[] args) {
+        int n = 14;
+
+        System.out.println(isUgly(n));
+    }
+    public static boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        int[] factors = {2,3,5};
+
+        for (int factor : factors) {
+
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+        
+        return n == 1;
+    }
+}
