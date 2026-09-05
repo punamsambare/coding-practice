205232e847554b37130fee2a94b701cc6ab2f57c
Punam Sambare <sambarepunam90@gmail.com>
2026-09-05 21:37:08 +0530

    LeetCode problem 441 Arranging Coins

---
+++ coding/Math/ArrangingCoins_441.java
@@ -0,0 +1,26 @@
+public class ArrangingCoins_441 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(arrangeCoins(n));
+    }
+
+    public static int arrangeCoins(int n) {
+        long left = 0, right = n;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long coinsUsed = mid * (mid + 1) / 2;
+
+            if (coinsUsed == n) {
+                return (int) mid;
+            } else if (coinsUsed < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return (int) right;
+    }
+}
