2f93d64510c2bccf4f8c08f4433e2e1c2d2d1cdd
Punam Sambare <sambarepunam90@gmail.com>
2026-09-11 20:30:36 +0530

    LeetCode problem 292 Nim Game

---
+++ coding/Math/NimGame_292.java
@@ -0,0 +1,12 @@
+public class NimGame_292 {
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        System.out.println(canWinNim(n));
+    }
+    
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
